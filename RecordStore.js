9d22de36b7e2848737819f6376a024167340a340
Colin McKean <colinmckeandigital@gmail.com>
2017-03-24 11:11:00 +0000

    pass record store test to add records to inventory

--- RecordStore.js
+++ RecordStore.js
@@ -5,4 +5,10 @@ RecordStore = function(name, city) {
   this.inventory = [];
 }
 
+RecordStore.prototype = {
+  add_to_inventory: function(record){
+    this.inventory.push(record);
+
+  }
+};
 module.exports = RecordStore;
