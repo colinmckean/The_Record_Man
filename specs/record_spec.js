5df4a3edf0dd63a0d1cf814dc9fd173ad7d0d447
Colin McKean <colinmckeandigital@gmail.com>
2017-03-24 10:48:50 +0000

    add test that record should have a title

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -5,5 +5,8 @@ describe('The Record', function () {
   var record = new Record("Colin", "a record", 10);
   it('should have an artist name of Colin', function (){
     assert.equal("Colin", record.artist);
+  });
+  it('should have an title of a record', function(){
+    assert.equal("a record", record.title);
   })
 })
