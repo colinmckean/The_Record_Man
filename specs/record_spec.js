ebe755cbb995f00c8b2242bdbd500fa760a153c6
Colin McKean <colinmckeandigital@gmail.com>
2017-03-26 19:56:47 +0100

    refactor and tidy up

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -2,17 +2,17 @@ var Record = require('../Record');
 var assert = require('assert');
 
 describe('The Record', function () {
-  var record = new Record("Colin", "a record", 10);
+  var record = new Record("Colin", "A Hit like a BaseBall Bat out of Hell.", 10);
   it('should have an artist name of Colin', function (){
     assert.equal("Colin", record.artist);
   });
-  it('should have an title of a record', function(){
-    assert.equal("a record", record.title);
+  it('should have an title of A Hit like a BaseBall Bat out of Hell.', function(){
+    assert.equal("A Hit like a BaseBall Bat out of Hell.", record.title);
   });
   it('should have a price of 10', function(){
     assert.equal(10, record.price);
   });
   it('should return a string of record details.', function () {
-    assert.equal("a record by Colin for only 10", record.record_details());
+    assert.equal("A Hit like a BaseBall Bat out of Hell. by Colin for only £10", record.record_details());
   });
 });
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -4,9 +4,9 @@ var assert  = require('assert');
 
 describe('The Record Store', function () {
   var store = new RecordStore("Colins Record Store", "Glasgow");
-  var record_1 = new Record("Colin", "a record", 10);
-  var record_2 = new Record("John", "another record", 15);
-  var record_3 = new Record("Alan", "a third record", 20);
+  var record_1 = new Record("Colin", "A Hit like a BaseBall Bat out of Hell.", 10);
+  var record_2 = new Record("John", "This is my Jam", 15);
+  var record_3 = new Record("Alan", "Aint no paddler like a ping pong paddler", 20);
   it('Should have a name', function () {
     assert.equal("Colins Record Store", store.name);
   });
@@ -22,17 +22,17 @@ describe('The Record Store', function () {
   it('should increase inventory size when a record is added', function () {
     store.add_to_inventory(record_1);
     assert.equal(1, store.inventory.length);
-    assert.deepEqual([{ artist: 'Colin', title: 'a record', price: 10 }], store.inventory);
+    assert.deepEqual([{ artist: 'Colin', title: 'A Hit like a BaseBall Bat out of Hell.', price: 10 }], store.inventory);
   });
   it('should print out all record details of stock in its inventory', function () {
     store.add_to_inventory(record_2);
-    assert.deepEqual([ 'a record by Colin for only 10',
-      'another record by John for only 15' ], store.list_inventory() );
+    assert.deepEqual([ 'A Hit like a BaseBall Bat out of Hell. by Colin for only £10',
+      'This is my Jam by John for only £15' ], store.list_inventory() );
   });
   it('should sell records to customers and increase balance', function(){
     store.sell_record(record_2);
     assert.equal(15, store.balance);
-    assert.deepEqual([ 'a record by Colin for only 10'], store.list_inventory() );
+    assert.deepEqual([ 'A Hit like a BaseBall Bat out of Hell. by Colin for only £10'], store.list_inventory() );
   });
   it('should provide a sales report', function(){
     assert.equal("STORE REPORT: \nFUNDS: 15\nSTOCK VALUE: 10", store.sit_rep());
