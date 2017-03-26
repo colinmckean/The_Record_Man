65397df95b4e9e6c7c4e22a81143c477a5492c87
Colin McKean <colinmckeandigital@gmail.com>
2017-03-26 20:46:25 +0100

    refactor and tidy up.

--- specs/record_collector_spec.js
+++ specs/record_collector_spec.js
@@ -3,34 +3,49 @@ var RecordCollector = require('../RecordCollector');
 var Record = require('../Record');
 
 describe('The RecordCollector', function(){
-  var customer = new RecordCollector();
-  var record_1 = new Record("Colin", "a record", 10);
-  var record_2 = new Record("Colin", "expensive tastes", 100);
+
+  var customer;
+  var record_1;
+  var record_2;
+
+  before(function(){
+
+    customer = new RecordCollector();
+    record_1 = new Record("Colin", "Poor Choice", 10);
+    record_2 = new Record("Colin", "expensive tastes", 100);
+  });
+
   it('should have an empty collection', function(){
     assert.equal(0, customer.record_collection.length);
     assert.deepEqual([], customer.record_collection);
   });
+
   it('should start with 50 cash', function () {
     assert.equal(50, customer.cash);
   });
+
   it('should increase record collection when buying a record', function(){
     customer.buy_record(record_1);
     assert.equal(1, customer.record_collection.length);
-    assert.deepEqual([ { artist: 'Colin', title: 'a record', price: 10 } ], customer.record_collection);
+    assert.deepEqual([ { artist: 'Colin', title: 'Poor Choice', price: 10 } ], customer.record_collection);
   });  
+
   it('should reduce funds when buying a record', function(){
     assert.equal(40, customer.cash);
   });  
+
   it('should not be able to buy a record if they do not have funds', function(){
     customer.buy_record(record_2);
     assert.equal(40, customer.cash);
     assert.equal(1, customer.record_collection.length);
-    assert.deepEqual([ { artist: 'Colin', title: 'a record', price: 10 } ], customer.record_collection);
+    assert.deepEqual([ { artist: 'Colin', title: 'Poor Choice', price: 10 } ], customer.record_collection);
   });
+
   it('should increase cash when selling a record', function(){
     customer.sell_record(record_1);
     assert.equal(50, customer.cash);
   });
+
   it('should remove record from collection once sold.', function() {
     assert.deepEqual([], customer.record_collection);
   });
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -3,38 +3,54 @@ var Record = require('../Record');
 var assert  = require('assert');
 
 describe('The Record Store', function () {
-  var store = new RecordStore("Colins Record Store", "Glasgow");
-  var record_1 = new Record("Colin", "A Hit like a BaseBall Bat out of Hell.", 10);
-  var record_2 = new Record("John", "This is my Jam", 15);
-  var record_3 = new Record("Alan", "Aint no paddler like a ping pong paddler", 20);
+
+    var store;
+    var record_1;
+    var record_2;
+    var record_3;
+
+  before(function () {
+    store = new RecordStore("Colins Record Store", "Glasgow");
+    record_1 = new Record("Colin", "A Hit like a BaseBall Bat out of Hell.", 10);
+    record_2 = new Record("John", "This is my Jam", 15);
+    record_3 = new Record("Alan", "Aint no paddler like a ping pong paddler", 20);
+  });
+  
   it('Should have a name', function () {
     assert.equal("Colins Record Store", store.name);
   });
+
   it('Should have a city', function () {
     assert.equal("Glasgow", store.city);
   });
+
   it('should have 0 balance to start', function () {
     assert.equal(0, store.balance);
   });
+
   it('should have an empty inventory to start', function () {
     assert.deepEqual([], store.inventory);
   });
+
   it('should increase inventory size when a record is added', function () {
     store.add_to_inventory(record_1);
     assert.equal(1, store.inventory.length);
     assert.deepEqual([{ artist: 'Colin', title: 'A Hit like a BaseBall Bat out of Hell.', price: 10 }], store.inventory);
   });
+
   it('should print out all record details of stock in its inventory', function () {
     store.add_to_inventory(record_2);
     assert.deepEqual([ 'A Hit like a BaseBall Bat out of Hell. by Colin for only £10',
       'This is my Jam by John for only £15' ], store.list_inventory() );
   });
-  it('should sell records to customers and increase balance', function(){
+
+  it('should sell records to customers and increase balance', function () {
     store.sell_record(record_2);
     assert.equal(15, store.balance);
     assert.deepEqual([ 'A Hit like a BaseBall Bat out of Hell. by Colin for only £10'], store.list_inventory() );
   });
-  it('should provide a sales report', function(){
+
+  it('should provide a sales report', function () {
     assert.equal("STORE REPORT: \nFUNDS: 15\nSTOCK VALUE: 10", store.sit_rep());
   })
 });
